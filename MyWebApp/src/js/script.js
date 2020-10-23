87a43a8a0209d78c2c9db3df54ce0363099e2f8f
David Naar <dnaar@uninorte.edu.co>
2020-10-23 17:44:27 -0500

    Actualización para simultaneidad de camiones

--- MyWebApp/src/js/script.js
+++ MyWebApp/src/js/script.js
@@ -1,17 +1,23 @@
 var map;
 var markers = [];
-var historicpath;
-var historicline;
-var filteredpath;
-var filtermarkers = [];
-var pathmarkers = [];
-var historicmarker;
-var addline = false;
-var vehicle = 1;
+
+
+var historicpath = [];
+var filteredpath = [];
+var filtermarkers = [, [],
+    []
+];
+var pathmarkers = [, [],
+    []
+];
+var historicmarker = [];
+
 const today_date = new Date();
 const today_boundaries = { start: new Date(today_date.getFullYear(), today_date.getMonth(), today_date.getDate()).getTime(), end: new Date(today_date.getFullYear(), today_date.getMonth(), today_date.getDate() + 1).getTime() };
 
-var selecting = false;
+const vehicle = { vehicle1: 1, vehicle2: 2 };
+var selected = [, true, false];
+var addline = [, false, false];
 
 
 // Inicialización de mapa
@@ -38,59 +44,71 @@ async function iniciarMap() {
     markers[1] = marker;
     var marker2 = L.marker([0, 0], { icon: truck2icon, interactive: false });
     markers[2] = marker2;
-    await updateMarker();
-    await _mostrarHistorial();
+    await _mostrarHistorial(vehicle.vehicle1);
+    await updateMarker(vehicle.vehicle1);
+    setInterval(() => {
+        updateMarker(vehicle.vehicle1);
+    }, 1000);
+    setInterval(() => {
+        updateMarker(vehicle.vehicle2);
+    }, 1000);
 }
 
 // Función de obtención de datos
-async function getData() {
-    // Obtener datos del listener remoto
+async function getData(vehicle) {
     const response = await fetch(`/loc/${vehicle}`, { method: 'GET' });
     const data = await response.json();
     return data;
 }
 
 // Actualizar marcador de localizacion
-async function updateMarker() {
-    try {
-        const _location = await getData();
-        const coord = { lat: _location.latitude, lng: _location.longitude };
-        markers[vehicle].setLatLng(coord).addTo(map);
-        if (addline && historicline.length > 0) {
-            const path = historicpath.getLatLngs();
-            path.push(markers[vehicle].getLatLng());
-            historicpath.setLatLngs(path);
-        }
-        _changeText(_location);
-    } catch (error) {
-        console.log(error);
+async function updateMarker(vehicle) {
+    if (!selected[vehicle]) {
+        return;
     }
-    setTimeout(updateMarker, 5000);
+    const _location = await getData(vehicle);
+    const coord = { lat: _location.latitude, lng: _location.longitude };
+    markers[vehicle].setLatLng(coord).addTo(map);
+    _changeText(_location, vehicle);
+    try {
+        if (addline[vehicle] && !historicpath[vehicle].isEmpty()) {
+            const path = historicpath[vehicle].getLatLngs();
+            path.push(markers[vehicle].getLatLng());
+            historicpath[vehicle].setLatLngs(path);
+        }
+    } catch (error) {
+        return;
+    }
 }
 // FF00E0   Color camion 2
 
 // Función de actualización de datos
-function _changeText(_location) {
+function _changeText(_location, vehicle) {
     datet = new Date(_location.timestamp)
-    document.getElementById("txtlat").innerHTML = _location.latitude;
-    document.getElementById("txtlong").innerHTML = _location.longitude;
-    document.getElementById("txtlumx").innerHTML = _location.lumx;
-    document.getElementById("txtaccel").innerHTML = _location.accel;
+    document.getElementById(`txtlat${vehicle}`).innerHTML = _location.latitude;
+    document.getElementById(`txtlong${vehicle}`).innerHTML = _location.longitude;
+    document.getElementById(`txtlumx${vehicle}`).innerHTML = _location.lumx;
+    document.getElementById(`txtaccel${vehicle}`).innerHTML = _location.accel;
     if (datet.getMinutes() < 10) {
-        document.getElementById("time_text").innerHTML = datet.getHours() + ":0" + datet.getMinutes();
+        document.getElementById(`time_text${vehicle}`).innerHTML = datet.getHours() + ":0" + datet.getMinutes();
     } else {
-        document.getElementById("time_text").innerHTML = datet.getHours() + ":" + datet.getMinutes();
+        document.getElementById(`time_text${vehicle}`).innerHTML = datet.getHours() + ":" + datet.getMinutes();
     }
-    document.getElementById("date_text").innerHTML = datet.getDate() + "/" + (datet.getMonth() + 1) + "/" + datet.getFullYear();
+    document.getElementById(`date_text${vehicle}`).innerHTML = datet.getDate() + "/" + (datet.getMonth() + 1) + "/" + datet.getFullYear();
 }
 // Función para centrar el mapa al marcador
 function centerMap() {
-    map.setView(markers[vehicle].getLatLng(), 16);
+    if (selected[1]) {
+        map.setView(markers[vehicle.vehicle1].getLatLng(), 16);
+    } else {
+        map.setView(markers[vehicle.vehicle2].getLatLng(), 16);
+    }
+
 }
 
-async function _mostrarHistorial() {
-    historicline = [];
-    addline = true;
+async function _mostrarHistorial(vehicle) {
+    var historicline = [];
+    addline[vehicle] = true;
     const options = {
         method: "POST",
         body: JSON.stringify(today_boundaries),
@@ -116,19 +134,20 @@ async function _mostrarHistorial() {
         } else if (vehicle == 2) {
             lcolor = '#FF00E0';
         }
-        historicmarker = L.marker(historicline[0], { icon: linestart }).bindPopup("<b>Inicio del recorrido del día.</b>").addTo(map);
-        historicpath = L.polyline(historicline, {
+        historicmarker[vehicle] = L.marker(historicline[0], { icon: linestart }).bindPopup("<b>Inicio del recorrido del día.</b>").addTo(map);
+        historicpath[vehicle] = L.polyline(historicline, {
             color: lcolor,
+            opacity: 0.8,
             lineCap: linestart,
             interactive: false
         });
-        historicpath.addTo(map);
+        historicpath[vehicle].addTo(map);
     }
 
 }
 
 // Filtrar el historial de datos
-async function updateintervaldate() {
+async function updateintervaldate(vehicle) {
     var polyline = [];
     var timespan = [];
     var sensordata = [];
@@ -182,38 +201,39 @@ async function updateintervaldate() {
         } else if (vehicle == 2) {
             lcolor = '#34d2eb';
         }
-        filtermarkers[0] = L.marker(polyline[0], { icon: linestart }).bindPopup("<b>Inicio del recorrido.</b>").addTo(map);
-        filtermarkers[1] = L.marker(polyline[polyline.length - 1], { icon: lineend }).bindPopup("<b>Fin del recorrido.</b>").addTo(map);
-        filteredpath = L.polyline(polyline, {
+        filtermarkers[vehicle][0] = L.marker(polyline[0], { icon: linestart }).bindPopup("<b>Inicio del recorrido.</b>").addTo(map);
+        filtermarkers[vehicle][1] = L.marker(polyline[polyline.length - 1], { icon: lineend }).bindPopup("<b>Fin del recorrido.</b>").addTo(map);
+        filteredpath[vehicle] = L.polyline(polyline, {
             color: lcolor,
+            opacity: 0.8,
             interactive: false
         }).addTo(map);
         const submarker = L.marker([0, 0], { icon: path_icon });
-        pathmarkers[0] = submarker;
-        mySlider = document.getElementById("pathing");
+        pathmarkers[vehicle][0] = submarker;
+        mySlider = document.getElementById(`pathing${vehicle}`);
         mySlider.min = `${0}`;
         mySlider.max = `${polyline.length - 1}`;
         mySlider.oninput = function() {
             var index = parseInt(this.value);
             const sliderdate = new Date(timespan[index]);
-            pathmarkers[0].setLatLng(polyline[index]).addTo(map);
-            date0 = document.getElementById("slidervalue0");
+            pathmarkers[vehicle][0].setLatLng(polyline[index]).addTo(map);
+            date0 = document.getElementById(`sliderdate${vehicle}`);
             if (sliderdate.getMinutes() < 10) {
                 date0.innerHTML = sliderdate.getDate() + "/" + (sliderdate.getMonth() + 1) + "/" + sliderdate.getFullYear() + " " + sliderdate.getHours() + ":0" + sliderdate.getMinutes();
             } else {
                 date0.innerHTML = sliderdate.getDate() + "/" + (sliderdate.getMonth() + 1) + "/" + sliderdate.getFullYear() + " " + sliderdate.getHours() + ":" + sliderdate.getMinutes();
             }
-            document.getElementById("slideraccel").innerHTML = sensordata[index].accel;
-            document.getElementById("sliderlumx").innerHTML = sensordata[index].lumx;
+            document.getElementById(`slideraccel${vehicle}`).innerHTML = sensordata[index].accel;
+            document.getElementById(`sliderlumx${vehicle}`).innerHTML = sensordata[index].lumx;
         }
 
     } else {
         if (time_interval.start >= time_interval.end) {
             alert("Por favor ingrese un rango de fechas válido.");
-            $(document.getElementById("slidercontainer")).slideToggle(0);
+            $(document.getElementById(`slidercontainer${vehicle}`)).slideToggle(0);
         } else {
             alert("No hay recorrido entre esas fechas.");
-            $(document.getElementById("slidercontainer")).slideToggle(0);
+            $(document.getElementById(`slidercontainer${vehicle}`)).slideToggle(0);
         }
         document.getElementById("filtrado").checked = false;
 
@@ -244,98 +264,174 @@ function checkingbox() {
 }
 checkFiltrado.addEventListener('change', function() {
     if (this.checked) {
-        updateintervaldate();
-        $(document.getElementById("slidercontainer")).slideToggle("fast");
+        if (selected[1]) {
+            updateintervaldate(vehicle.vehicle1);
+            $(document.getElementById("slidercontainer1")).slideDown("fast");
+        }
+        if (selected[2]) {
+            updateintervaldate(vehicle.vehicle2);
+            $(document.getElementById("slidercontainer2")).slideDown("fast");
+        }
     } else {
-        clearintervaldate();
-        $(document.getElementById("slidercontainer")).slideToggle("fast");
+        $(document.getElementById("slidercontainer1")).slideUp("fast");
+        $(document.getElementById("slidercontainer2")).slideUp("fast");
+        if (selected[1]) {
+            clearintervaldate(vehicle.vehicle1);
+        }
+        if (selected[2]) {
+            clearintervaldate(vehicle.vehicle2);
+        }
     }
 });
 
-function clearintervaldate() {
-    filteredpath.remove();
-    filtermarkers[0].remove();
-    filtermarkers[1].remove();
-    pathmarkers[0].remove();
+function clearintervaldate(vehicle) {
+    filteredpath[vehicle].remove();
+    filtermarkers[vehicle][0].remove();
+    filtermarkers[vehicle][1].remove();
+    pathmarkers[vehicle][0].remove();
 }
 
-const checkHistorial = document.getElementById("historial");
-checkHistorial.addEventListener('change', function() {
+const checkHistorial1 = document.getElementById("historial1");
+const checkHistorial2 = document.getElementById("historial2");
+checkHistorial1.addEventListener('change', function() {
     if (this.checked) {
-        _mostrarHistorial();
-    } else {
-        if (historicline.length > 0) {
-            historicpath.remove();
-            historicmarker.remove();
+        if (selected[1]) {
+            addline[1] = true;
+            _mostrarHistorial(vehicle.vehicle1);
         }
-        addline = false;
+    } else {
+        if (selected[1]) {
+            addline[1] = false;
+            try {
+                if (!historicpath[1].isEmpty()) {
+                    historicpath[1].remove();
+                    historicmarker[1].remove();
+                }
+            } catch (e) {}
+        }
+    }
+});
+checkHistorial2.addEventListener('change', function() {
+    if (this.checked) {
+        if (selected[2]) {
+            addline[2] = true;
+            _mostrarHistorial(vehicle.vehicle2);
+        }
+    } else {
+        if (selected[2]) {
+            try {
+                if (!historicpath[2].isEmpty()) {
+                    historicpath[2].remove();
+                    historicmarker[2].remove();
+                }
+            } catch (e) {}
+        }
     }
 });
 
 async function v1selected() {
-    while (selecting) {}
-    selecting = true;
-    vehicle = 1;
+    selected[1] = !selected[1];
+    addline[1] = !addline[1];
+    document.getElementById("ubicacion2").style.borderTop = "";
     $(document.getElementById("options")).slideToggle("fast");
-    document.getElementById("selectHeader").innerHTML = "Camión 1";
-    markers[2].remove();
-    addline = false;
-    if (checkHistorial.checked) {
-        if (historicline.length > 0) {
-            historicpath.remove();
-            historicmarker.remove();
+    if (selected[1]) {
+        $(document.getElementById("ubicacion_padding")).fadeIn("fast");
+        $(document.getElementById("filter_padding")).fadeIn("fast");
+        $(document.getElementById("ubicacion1")).slideDown("fast");
+        updateMarker(vehicle.vehicle1);
+        if (selected[2]) {
+            document.getElementById("selectHeader").innerHTML = "Camión 1 y Camión 2";
+            document.getElementById("ubicacion2").style.borderTop = "5px solid #feffff";
+        } else {
+            document.getElementById("selectHeader").innerHTML = "Camión 1";
         }
-        await _mostrarHistorial();
+        if (checkHistorial1.checked) {
+            _mostrarHistorial(vehicle.vehicle1);
+        }
+        if (checkFiltrado.checked) {
+            updateintervaldate(vehicle.vehicle1);
+            $(document.getElementById("slidercontainer1")).slideDown("fast");
+        }
+        document.getElementById("checked1").style.display = "inline";
     } else {
-        if (historicline.length > 0) {
-            historicpath.remove();
-            historicmarker.remove();
+        document.getElementById("checked1").style.display = "none";
+        $(document.getElementById("ubicacion1")).slideUp("fast");
+        markers[1].remove();
+        if (selected[2]) {
+            document.getElementById("selectHeader").innerHTML = "Camión 2";
+        } else {
+            document.getElementById("selectHeader").innerHTML = "Elige un camión";
+            $(document.getElementById("ubicacion_padding")).fadeOut("fast");
+            $(document.getElementById("filter_padding")).fadeOut("fast");
         }
-        addline = false;
+        if (checkFiltrado.checked) {
+            clearintervaldate(vehicle.vehicle1);
+            $(document.getElementById("slidercontainer1")).slideUp("fast");
+        }
+        if (checkHistorial1.checked) {
+            try {
+                if (!historicpath[1].isEmpty()) {
+                    historicpath[1].remove();
+                    historicmarker[1].remove();
+                }
+            } catch (e) {}
+        }
     }
-    if (document.getElementById("filtrado").checked) {
-        clearintervaldate();
-        await updateintervaldate();
-    }
-    await updateMarker();
-    document.getElementById("checked1").style.display = "inline";
-    document.getElementById("checked2").style.display = "none";
-    selecting = false;
 }
 
 async function v2selected() {
-    while (selecting) {}
-    selecting = true;
-    vehicle = 2;
+    selected[2] = !selected[2];
+    addline[2] = !addline[2];
+    document.getElementById("ubicacion2").style.borderTop = "";
     $(document.getElementById("options")).slideToggle("fast");
-    document.getElementById("selectHeader").innerHTML = "Camión 2";
-    markers[1].remove();
-    addline = false;
-    if (checkHistorial.checked) {
-        if (historicline.length > 0) {
-            historicpath.remove();
-            historicmarker.remove();
+    if (selected[2]) {
+        $(document.getElementById("ubicacion_padding")).fadeIn("fast");
+        $(document.getElementById("filter_padding")).fadeIn("fast");
+        $(document.getElementById("ubicacion2")).slideDown("fast");
+        updateMarker(vehicle.vehicle2);
+        if (selected[1]) {
+            document.getElementById("selectHeader").innerHTML = "Camión 1 y Camión 2";
+            document.getElementById("ubicacion2").style.borderTop = "5px solid #feffff";
+        } else {
+            document.getElementById("selectHeader").innerHTML = "Camión 2";
         }
-        await _mostrarHistorial();
+        if (checkHistorial2.checked) {
+            _mostrarHistorial(vehicle.vehicle2);
+        }
+        if (checkFiltrado.checked) {
+            updateintervaldate(vehicle.vehicle2);
+            $(document.getElementById("slidercontainer2")).slideDown("fast");
+        }
+        document.getElementById("checked2").style.display = "inline";
     } else {
-        if (historicline.length > 0) {
-            historicpath.remove();
-            historicmarker.remove();
+        document.getElementById("checked2").style.display = "none";
+        $(document.getElementById("ubicacion2")).slideUp("fast");
+        markers[2].remove();
+        if (selected[1]) {
+            document.getElementById("selectHeader").innerHTML = "Camión 1";
+        } else {
+            document.getElementById("selectHeader").innerHTML = "Elige un camión";
+            $(document.getElementById("ubicacion_padding")).fadeOut("fast");
+            $(document.getElementById("filter_padding")).fadeOut("fast");
         }
-        addline = false;
+        if (checkFiltrado.checked) {
+            clearintervaldate(vehicle.vehicle2);
+            $(document.getElementById("slidercontainer2")).slideUp("fast");
+        }
+        if (checkHistorial2.checked) {
+            try {
+                if (!historicpath[2].isEmpty()) {
+                    historicpath[2].remove();
+                    historicmarker[2].remove();
+                }
+            } catch (e) {}
+        }
     }
-    if (document.getElementById("filtrado").checked) {
-        clearintervaldate();
-        await updateintervaldate();
-    }
-    await updateMarker();
-    document.getElementById("checked1").style.display = "none";
-    document.getElementById("checked2").style.display = "inline";
-    selecting = false;
 }
 
+
+
+
 function showoptions() {
-    if (!selecting) {
-        $(document.getElementById("options")).slideToggle("fast");
-    }
+    $(document.getElementById("options")).slideToggle("fast");
 }
